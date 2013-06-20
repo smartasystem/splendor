259d7e0b2ce52317ea0bfbbd1b7713cfdd2d8158
Kristian Erendi <kristian@reptilo.se>
2013-06-20 19:36:39 +0200

    css changes

--- wp-content/themes/splendor/template-biz.php
+++ wp-content/themes/splendor/template-biz.php
@@ -8,37 +8,35 @@
  * @package WooFramework
  * @subpackage Template
  */
+global $woo_options, $wp_query;
+get_header();
 
- global $woo_options, $wp_query; 
- get_header();
- 
- $page_template = woo_get_page_template();
+$page_template = woo_get_page_template();
 ?>
-    <!-- #content Starts -->
-	<?php woo_content_before(); ?>
-    <div id="content" class="col-full business">
-    
-    	<div id="main-sidebar-container">
+<!-- #content Starts -->
+<?php woo_content_before(); ?>
+<div id="content" class="col-full business">
 
-			<?php if ( $woo_options['woo_slider_biz'] == 'true' ) { $saved = $wp_query; woo_slider_biz(); $wp_query = $saved; } ?>
-            <!-- #main Starts -->
-            <?php woo_main_before(); woo_breadcrumbs();?>
-            <div id="main">      
-<?php
-	woo_loop_before();
-  get_template_part( 'loop', 'blog' );
-	woo_loop_after();
-?>        
-            </div><!-- /#main -->
-            <?php woo_main_after(); ?>
-    
-			<?php get_sidebar(); ?>
-            
-		</div><!-- /#main-sidebar-container -->         
+  <div id="main-sidebar-container">
 
-		<?php get_sidebar( 'alt' ); ?>
-
-    </div><!-- /#content -->
-	<?php woo_content_after(); ?>
-    
+    <?php if ($woo_options['woo_slider_biz'] == 'true') {
+      $saved = $wp_query;
+      woo_slider_biz();
+      $wp_query = $saved;
+    } ?>
+    <!-- #main Starts -->
+    <!-- ?php woo_main_before(); woo_breadcrumbs();?-->
+    <div id="main">      
+      <?php
+      woo_loop_before();
+      get_template_part('loop', 'blog');
+      woo_loop_after();
+      ?>        
+    </div><!-- /#main -->
+<?php woo_main_after(); ?>
+  <?php get_sidebar(); ?>
+  </div><!-- /#main-sidebar-container -->         
+<?php get_sidebar('alt'); ?>
+</div><!-- /#content -->
+<?php woo_content_after(); ?>
 <?php get_footer(); ?>
